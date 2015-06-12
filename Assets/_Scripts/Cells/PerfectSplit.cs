9e375bb3f48c46b07051a7064003697bb8ae34a4
Junshu Chen <locpk123@gmail.com>
2015-06-12 15:49:39 -0400

    Put split functionality inside base cell

--- Assets/_Scripts/Cells/PerfectSplit.cs
+++ Assets/_Scripts/Cells/PerfectSplit.cs
@@ -4,27 +4,18 @@ using System.Collections.Generic;
 
 public class PerfectSplit : MonoBehaviour {
 
-
+    GameObject originalCell;
 	void Awake() {
-        
+        originalCell = gameObject;
     }
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-	void FixedUpdate() {
-       
-    }
+    void Split()
+    {
 
-	//LateUpdate is called after all Update functions have been called
-	void LateUpdate() {
-        
     }
 }
