42f529e9d784771dc284557d1ba9366361de3207
Damien <mullen.damien@gmail.com>
2015-06-12 14:18:28 -0400

    basic layout

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ToggleFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+}
