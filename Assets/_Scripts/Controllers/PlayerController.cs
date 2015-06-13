c9e9639adbdfc17f05644355bfcc21c84b512260
Damien <mullen.damien@gmail.com>
2015-06-13 14:18:23 -0400

    Lots of comments

--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -19,53 +19,33 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+        // Initialize variables
         selectedTarget = null;
         groups = new System.Collections.Generic.List<BaseCell>[10];
         allSelectableUnits = new System.Collections.Generic.List<BaseCell>();
         selectedUnits = new System.Collections.Generic.List<BaseCell>();
-        GameObject[] tmpArr = GameObject.FindGameObjectsWithTag("Unit");
-        foreach (GameObject item in tmpArr)
+        // ----------
+
+        GameObject[] tmpArr = GameObject.FindGameObjectsWithTag("Unit"); // Get every cell in the game
+        foreach (GameObject item in tmpArr) // Iterate through all the cells
         {
-            BaseCell bCell = item.GetComponent<BaseCell>();
-            if (!bCell.isAIPossessed && bCell.isMine)
+            BaseCell bCell = item.GetComponent<BaseCell>(); // Upcast each cell to a base cell
+            if (!bCell.isAIPossessed && bCell.isMine) // If the cell belongs to this player
             {
-                allSelectableUnits.Add(item.GetComponent<BaseCell>());
+                allSelectableUnits.Add(item.GetComponent<BaseCell>()); // Add the cell to the players controllable units
             }
         }
         selectionRect = new Rect();
     }
 
-    void MouseFeedback()
-    {
-        RaycastHit hit;
-        Ray mousetoWorldRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(mousetoWorldRay, out hit, 2000))
-        {
-            //if (hit.collider.name == "Test_Ground")
-            //{
-            //    Debug.DrawRay(mousetoWorldRay.origin, mousetoWorldRay.direction * 1000);
-            //    if (Input.GetMouseButtonDown(0))
-            //    {
-            //        //GameObject.Instantiate(attackPin, hit.point, Quaternion.identity);
-            //    }
-            //    if (Input.GetMouseButtonDown(1))
-            //    {
-            //      // GameObject.Instantiate(movePin, hit.point, Quaternion.identity);
-            //    }
-
-            //}
-        }
-    }
-
     public System.Collections.Generic.List<GameObject> GetAllSelectableObjects()
     {
-        System.Collections.Generic.List<GameObject> allSelectableObjects = new System.Collections.Generic.List<GameObject>();
-        foreach (BaseCell item in allSelectableUnits)
+        System.Collections.Generic.List<GameObject> allSelectableObjects = new System.Collections.Generic.List<GameObject>(); // Initialize a list of GameObjects
+        foreach (BaseCell item in allSelectableUnits) // For each of the player's controllable cells
         {
-            allSelectableObjects.Add(item.gameObject);
+            allSelectableObjects.Add(item.gameObject); // Add the cell's GameObject to the list
         }
-        return allSelectableObjects;
+        return allSelectableObjects; // Return the list
     }
 
     public void UnitSelection()
@@ -75,32 +55,32 @@ public class PlayerController : MonoBehaviour
 
     public void UnitMove()
     {
-        foreach (BaseCell item in selectedUnits)
+        foreach (BaseCell item in selectedUnits) // For each of the player's selected units
         {
-            item.Move(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            item.Move(Camera.main.ScreenToWorldPoint(Input.mousePosition)); // Set their destination
         }
     }
 
     public void UnitAttack()
     {
-        foreach (BaseCell item in selectedUnits)
+        foreach (BaseCell item in selectedUnits) // For each of the player's selected units
         {
-            item.Attack(selectedTarget);
+            item.Attack(selectedTarget); // Set the target cell to attack
         }
     }
 
     public void UnitSplit()
     {
-        foreach (BaseCell item in selectedUnits)
+        foreach (BaseCell item in selectedUnits) // For each of the player's selected units
         {
-            switch (item.celltype)
+            switch (item.celltype) // Dependent on the type of cell it is
             {
-                case CellType.STEM_CELL:
-                    item.PerfectSplit();
+                case CellType.STEM_CELL: // If it is a stem cell
+                    item.PerfectSplit(); // Split without a chance of cancer
                     break;
-                case CellType.HEAT_CELL:
-                case CellType.COLD_CELL:
-                    item.CancerousSplit();
+                case CellType.HEAT_CELL: // If it is a heat cell
+                case CellType.COLD_CELL: // OR If it is a cold cell
+                    item.CancerousSplit(); // Split with a chance of cancer
                     break;
 
                 default:
@@ -115,9 +95,9 @@ public class PlayerController : MonoBehaviour
 
     public void UnitHarvest()
     {
-        foreach (BaseCell item in selectedUnits)
+        foreach (BaseCell item in selectedUnits) // For each of the player's selected units
         {
-            item.Consume(selectedTarget);
+            item.Consume(selectedTarget); // Set the target protein to consume
         }
     }
 
@@ -127,14 +107,14 @@ public class PlayerController : MonoBehaviour
 
     public void DoubleClick()
     {
-        BaseCell selectedCell = selectedUnits[0].GetComponent<BaseCell>();
-        CellType selectedType = selectedCell.celltype;
-        selectedUnits.Clear();
-        foreach (BaseCell item in allSelectableUnits)
+        BaseCell selectedCell = selectedUnits[0].GetComponent<BaseCell>(); // Get double-clicked cell;
+        CellType selectedType = selectedCell.celltype; // Get the type of cell it is
+        selectedUnits.Clear(); // Remove control of any currently selected units
+        foreach (BaseCell item in allSelectableUnits) // For each of the player's selectable units
         {
-            if (item.celltype == selectedType)
+            if (item.celltype == selectedType) // If the type matches the double-clicked cell
             {
-                selectedUnits.Add(item);
+                selectedUnits.Add(item); // Add the cell to the players selected units
             }
         }
     }
@@ -161,7 +141,7 @@ public class PlayerController : MonoBehaviour
 
     public void UnitStop()
     {
-        foreach (BaseCell item in selectedUnits)
+        foreach (BaseCell item in selectedUnits) // For each of the player's selected units
         {
             //item.Stop();
         }
@@ -170,17 +150,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("D"))
+        if (Input.GetKeyDown("D")) // If the player presses D
         {
             UnitSplit();
         }
 
-        if (Input.GetKeyDown("S"))
+        if (Input.GetKeyDown("S")) // If the player presses S
         {
             UnitStop();
         }
 
-        if (Input.GetKeyDown("C"))
+        if (Input.GetKeyDown("C")) // If the player presses C
+        {
+            foreach (StemCell item in selectedUnits) // For each of the player's selected units
+            {
+                //item.Mutate(CellType.ACIDIC_CELL)
+            }
+        }
+
+        if (Input.GetKeyDown("X")) // If the player presses X
+        {
+            foreach (StemCell item in selectedUnits) // For each of the player's selected units
+            {
+                //item.Mutate(CellType.HEAT_CELL)
+            }
+        }
+
+        if (Input.GetKeyDown("V")) // If the player presses V
         {
             foreach (StemCell item in selectedUnits)
             {
@@ -188,30 +184,23 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown("X"))
+        if (Input.GetKeyDown("Z")) // If the player presses Z
         {
-
+            foreach (StemCell item in selectedUnits)
+            {
+                //item.Mutate(CellType.COLD_CELL)
+            }
         }
 
-        if (Input.GetKeyDown("V"))
-        {
-
-        }
-
-        if (Input.GetKeyDown("Z"))
-        {
-
-        }
-
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)) // If the player left-clicks
         {
             selectionRect.position.Set(Input.mousePosition.x, Input.mousePosition.y);
         }
-        else if (Input.GetMouseButtonUp(1))
+        else if (Input.GetMouseButtonUp(1)) // When the player releases left-click
         {
 
         }
-        else if (Input.GetMouseButton(1))
+        else if (Input.GetMouseButton(1)) // If the player has left-click held down
         {
             UnitSelection();
         }
