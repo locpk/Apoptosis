c419ba8f680db1cbfd8017468b9bf66136683f11
Devin Dellamano <devin_dellamano@yahoo.com>
2015-06-17 18:12:42 -0400

    Included Pause Menu
    Included the pause menu prefab into the singleplayer level 1

--- Assets/_Scripts/Menus/PauseMenu.cs
+++ Assets/_Scripts/Menus/PauseMenu.cs
@@ -5,44 +5,67 @@ public class PauseMenu : MonoBehaviour {
 
     //bool for if the game is paused or not
 	bool isPaused = false;
+    bool OnPause = false;
+    bool OptionsOn = false;
+    bool InstructOn = false;
     //get the pause menu
     public GameObject pauseMenu;
-    public GameObject optionsMenu;
-    public GameObject instructionsMenu;
+    //public GameObject optionsMenu;
+    //public GameObject instructionsMenu;
 
 
 	void Awake() {
         isPaused = false;
         pauseMenu.SetActive(false);
-        optionsMenu.SetActive(false);
-        instructionsMenu.SetActive(false);
+        //optionsMenu.SetActive(false);
+        //instructionsMenu.SetActive(false);
 	}
 
 	// Use this for initialization
 	void Start () {
         isPaused = false;
         pauseMenu.SetActive(false);
-        optionsMenu.SetActive(false);
-        instructionsMenu.SetActive(false);
+        //optionsMenu.SetActive(false);
+        //instructionsMenu.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape)) {
             isPaused = !isPaused;
+            OnPause = !OnPause;
 			//Debug.Log ("game paused");
 		}
 
         //check if game is paused or not
-		if (isPaused) {
+		if (isPaused && OnPause && !OptionsOn && !InstructOn) {
 			//Debug.Log ("game paused");
             //set the pause menu to true
             pauseMenu.SetActive(true);
 			Time.timeScale = 0.0f;
-		} else {
+		}
+        else if (isPaused && !OnPause && OptionsOn && !InstructOn)
+        {
+            pauseMenu.SetActive(false);
+            //optionsMenu.SetActive(true);
+            //instructionsMenu.SetActive(false);
+        }
+        else if (isPaused && !OnPause && !OptionsOn && InstructOn)
+        {
+            pauseMenu.SetActive(false);
+            //optionsMenu.SetActive(false);
+            //instructionsMenu.SetActive(true);
+        }
+        else if (!isPaused /*&& !OnPause && !OptionsOn && !InstructOn*/)
+        {
 			//Debug.Log ("game resumed");
             //turn the pause menu off
             pauseMenu.SetActive(false);
+            //optionsMenu.SetActive(true);
+            //instructionsMenu.SetActive(false);
+            InstructOn = false;
+            OptionsOn = false;
+            OnPause = false;
 			Time.timeScale = 1.0f;
 		}
 	
@@ -51,6 +74,10 @@ public class PauseMenu : MonoBehaviour {
     public void ContinuePlaying(){
         //set the bool to continue the game
         isPaused = false;
+        InstructOn = false;
+        OptionsOn = false;
+        OnPause = false;
+
     }
 
     public void RestartLevel()
@@ -61,14 +88,16 @@ public class PauseMenu : MonoBehaviour {
 
     public void LoadOptions()
     {
-        pauseMenu.SetActive(false);
-        optionsMenu.SetActive(true);
+        OnPause = false;
+        OptionsOn = true;
+        InstructOn = false;
     }
 
     public void LoadInstructions()
     {
-        pauseMenu.SetActive(false);
-        instructionsMenu.SetActive(true);
+        OnPause = false;
+        OptionsOn = false;
+        InstructOn = true;
     }
 
     public void ExitToMainMenu()
@@ -81,7 +110,7 @@ public class PauseMenu : MonoBehaviour {
         //Any variables that need to be reset should be reset in this function
         isPaused = false;
         pauseMenu.SetActive(false);
-        optionsMenu.SetActive(false);
-        instructionsMenu.SetActive(false);
+        //optionsMenu.SetActive(false);
+        //instructionsMenu.SetActive(false);
     }
 }
