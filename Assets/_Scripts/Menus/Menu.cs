eab9dc97cc8794c587c549c37b60670a6a6bb186
dmitrii <dvzyrianov@fullsail.edu>
2015-07-06 23:51:19 -0400

    Updated Main Menu with effects

--- Assets/_Scripts/Menus/Menu.cs
+++ Assets/_Scripts/Menus/Menu.cs
@@ -6,7 +6,16 @@ public class Menu : MonoBehaviour {
 
     //public GameObject menu;
 
-	void Awake() {
+    public GameObject backGround;
+    public GameObject loadingBar;
+
+    private int loadProgress = 0;
+
+	void Awake() 
+    {
+
+        backGround.SetActive(false);
+        loadingBar.SetActive(false);
     }
 
 	// Use this for initialization
@@ -31,7 +40,11 @@ public class Menu : MonoBehaviour {
     public void LoadScene(string SceneName)
     {
         //loading screen goes here, fade and put it in 
+
+        StartCoroutine(DisplayLoadingScreen(SceneName));
+
         Application.LoadLevel(SceneName);
+
     }
 
     public void ExitApplication()
@@ -39,4 +52,16 @@ public class Menu : MonoBehaviour {
         System.Diagnostics.Process.GetCurrentProcess().Kill();
       
     }
+
+    // this does the loading bar and returns when done
+    IEnumerator DisplayLoadingScreen(string levelName)
+    {
+        loadingBar.transform.localScale = new Vector3(loadProgress, loadingBar.transform.position.x, loadingBar.transform.position.z);
+   
+        backGround.SetActive(true);
+        loadingBar.SetActive(true);
+
+        yield return null; 
+    }
+
 }
