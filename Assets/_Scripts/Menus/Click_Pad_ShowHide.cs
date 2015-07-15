e6e2fb2341c6f3e1139ef828f2abeaf813fefaf9
dmitrii <dvzyrianov@fullsail.edu>
2015-07-15 16:51:39 -0400

    WorkingFinger pad

---
+++ Assets/_Scripts/Menus/Click_Pad_ShowHide.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click_Pad_ShowHide : MonoBehaviour {
+
+	// Use this for initialization
+    public Animator animator;
+    public bool show;
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Show_Hide_Panel()
+    {
+       
+            animator.SetBool("SHOW", show);
+            show = !show;
+     
+       
+    }
+}
