ae0cdd6cf06315d66576c05f05f237f92a87a791
Paspy <ipaspy@gmail.com>
2015-07-23 19:54:11 -0400

    Fixed a bug from delayed trap spawner

--- Assets/_Scripts/Cells/AlkaliCell.cs
+++ Assets/_Scripts/Cells/AlkaliCell.cs
@@ -19,7 +19,7 @@ public class AlkaliCell : BaseCell
         InvokeRepeating("MUltiDMg", 1.0f, 1.0f);
 
         sound_manager = GameObject.FindGameObjectWithTag("Sound_Manager").GetComponent<Sound_Manager>();
-        pcontroller = base.pcontroller;
+        //pcontroller = base.pcontroller;
     }
    
     void MUltiDMg() {
