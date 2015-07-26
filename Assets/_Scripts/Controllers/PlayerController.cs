5ee41c73f72082616a4504a9aecf41049b6336f0
Junshu Chen <locpk123@gmail.com>
2015-07-25 20:43:12 -0400

    BUG_FIX#6 •Protein script handles  put Protein to allSelectableTargets List •Fixed initial cells that are not being put into selectedUnits List •Made wait for second player gui •Optimized some codes

--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -107,39 +107,14 @@ public class PlayerController : MonoBehaviour
 
 
         touchButton = GameObject.Find("Touch");
+        if (!Input.touchSupported)
+        {
+            GameObject.Find("RIGHT_CLICK_Panel").SetActive(false);
+            isSelecting = false;
+        }
 
-
-        // Initialize variables
-        selectedTargets.Clear();
-        //        groups = new List<BaseCell>[10];
-        allSelectableUnits = new List<BaseCell>();
-        selectedUnits = new List<BaseCell>();
         terrainLayer = 1 << LayerMask.NameToLayer("Terrain");  // Layer masking for raycast clicking
         // ----------
-
-        List<GameObject> tmpArr = GameObjectManager.FindAllUnits(); // Get every cell in the game
-        foreach (GameObject item in tmpArr) // Iterate through all the cells
-        {
-            BaseCell bCell = item.GetComponent<BaseCell>(); // Upcast each cell to a base cell
-            if (bCell)
-            {
-                if (PhotonNetwork.connected && item.GetPhotonView().isMine || !bCell.isAIPossessed && bCell.isMine) // If the cell belongs to this player
-                {
-                    allSelectableUnits.Add(item.GetComponent<BaseCell>()); // Add the cell to the players controllable units
-                }
-                else if (PhotonNetwork.connected && !item.GetPhotonView().isMine || bCell.isAIPossessed && !bCell.isMine) // If the cell belongs to this player
-                {
-                    allSelectableTargets.Add(item); // Add the cell to the players controllable units
-                }
-            }
-        }
-
-        tmpArr.Clear();
-        tmpArr = GameObject.FindGameObjectsWithTag("Protein").ToList<GameObject>(); // Get every cell in the game
-        foreach (GameObject item in tmpArr) // Iterate through all the cells
-        {
-            allSelectableTargets.Add(item); // Add the cell to the players controllable units
-        }
     }
 
 
@@ -498,7 +473,7 @@ public class PlayerController : MonoBehaviour
         {
             if (!isOverUI)
             {
-                if (isSelecting)
+                if (isSelecting || Input.GetMouseButton(0))
                 {
                     GUI.color = new Color(0.0f, 0.0f, 1.0f, 0.5f);
                 }
@@ -512,7 +487,7 @@ public class PlayerController : MonoBehaviour
 
         }
         float scale = .7f;
-        GUI.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        //GUI.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
         foreach (GameObject item in selectedTargets)
         {
             if (item && selectedUnits.Count > 0)
@@ -583,24 +558,20 @@ public class PlayerController : MonoBehaviour
 
     public void FixedUpdate()
     {
-        CheckSelectedUnits();
-        CheckEnemiesLeft();
+        
 
-        if (isSelecting)
+        if (isSelecting && Input.touchSupported)
         {
             touchButton.GetComponent<Button>().image.sprite = touchButton.GetComponent<Button>().spriteState.pressedSprite;
 
         }
-        else
+        else if (Input.touchSupported)
         {
             touchButton.GetComponent<Button>().image.sprite = touchButton.GetComponent<Button>().spriteState.disabledSprite;
         }
 
 
-        if (allSelectableUnits.Count == 0 && gameStarted)
-        {
-            Show_LoseScreen();
-        }
+        
 
         
     }
@@ -935,7 +906,7 @@ public class PlayerController : MonoBehaviour
         }
     }
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         fps = 1.0f / Time.deltaTime;
         cap = allSelectableUnits.Count;
@@ -953,21 +924,18 @@ public class PlayerController : MonoBehaviour
             MouseKeyBoardUpdate();
         }
 
-
+        CheckSelectedUnits();
+        CheckEnemiesLeft();
+        if (allSelectableUnits.Count == 0 && gameStarted)
+        {
+            Show_LoseScreen();
+        }
 
 
     }
 
     public void CheckSelectedUnits()
     {
-        NumStemCells = 0;
-        NumHeatCells = 0;
-        NumColdCells = 0;
-        NumAcidicCells = 0;
-        NumAlkaliCells = 0;
-        NumTierTwoCold = 0;
-        NumTierTwoHeat = 0;
-
         NumStemCells = selectedUnits.FindAll(item => (item != null) && (item.celltype == CellType.STEM_CELL)).Count;
         NumHeatCells = selectedUnits.FindAll(item => (item != null) && (item.celltype == CellType.HEAT_CELL)).Count;
         NumColdCells = selectedUnits.FindAll(item => (item != null) && (item.celltype == CellType.COLD_CELL)).Count;
@@ -980,7 +948,6 @@ public class PlayerController : MonoBehaviour
 
     public void CheckEnemiesLeft()
     {
-        NumEnemiesLeft = 0;
 
         List<GameObject> enemies = new List<GameObject>(allSelectableTargets);
 
