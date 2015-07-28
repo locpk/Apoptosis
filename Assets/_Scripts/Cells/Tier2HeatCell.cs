f3932322123a5f4ef3e0b0c7082ca5d4bfcdf1cc
Noctis Vaeixia <noctisvaneixia@gmail.com>
2015-07-28 18:07:57 -0400

    minor  changes

--- Assets/_Scripts/Cells/Tier2HeatCell.cs
+++ Assets/_Scripts/Cells/Tier2HeatCell.cs
@@ -198,7 +198,10 @@ public class Tier2HeatCell : BaseCell
                     base.bUpdate();
                     break;
                 case CellState.MOVING:
-                    
+                    if (IsInvoking("DamagePerSecond"))
+                    {
+                        CancelInvoke("DamagePerSecond");
+                    }
                     base.bUpdate();
                     break;
                 case CellState.ATTACK_MOVING:
