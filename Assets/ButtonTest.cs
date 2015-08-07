a4d00b141b457144233708df10f384fe088c07c3
dmitrii <dvzyrianov@fullsail.edu>
2015-08-07 14:56:13 -0400

    HUD buttons respond to selected cells

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour {
+
+    public Button button;
+    
+	// Use this for initialization
+	void Start () {
+        button.GetComponent<Button>().interactable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
